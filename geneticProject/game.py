5bbdfeaba3ecd87bfaa469918d9adafc98367523
unknown <arminzd1@gmail.com>
2021-05-30 19:57:28 +0430

    start ...

---
+++ geneticProject/game.py
@@ -0,0 +1,41 @@
+class Game:
+    def __init__(self, levels):
+        # Get a list of strings as levels
+        # Store level length to determine if a sequence of action passes all the steps
+
+        self.levels = levels
+        self.current_level_index = -1
+        self.current_level_len = 0
+
+    def load_next_level(self):
+        self.current_level_index += 1
+        self.current_level_len = len(self.levels[self.current_level_index])
+
+    def get_score(self, actions):
+        # Get an action sequence and determine the steps taken/score
+        # Return a tuple, the first one indicates if these actions result in victory
+        # and the second one shows the steps taken
+
+        failure_points = []
+        current_level = self.levels[self.current_level_index]
+        steps = 0
+        for i in range(self.current_level_len - 1):
+            current_step = current_level[i]
+            if (current_step == '_'):
+                steps += 1
+            elif (current_step == 'G' and actions[i - 1] == '1'):
+                steps += 1
+            elif (current_step == 'L' and actions[i - 1] == '2'):
+                steps += 1
+            else:
+                failure_points.append(i)
+        return failure_points
+        # return steps == self.current_level_len - 1, steps
+
+
+# ddd = , "___G_M___L_"
+g = Game(["____G__L__"])
+g.load_next_level()
+
+# This outputs (False, 4)
+print(g.get_score("0000000000"))
