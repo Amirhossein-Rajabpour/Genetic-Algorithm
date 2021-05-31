d248684eee2d6019e5399bd4a171bcce9e28599b
unknown <arminzd1@gmail.com>
2021-06-01 01:14:03 +0430

    add some functiuons for GUI

--- geneticProject/GraphicalUserInterface.py
+++ geneticProject/GraphicalUserInterface.py
@@ -1,8 +1,54 @@
 import tkinter as tk
 from tkinter import ttk
 from PIL import ImageTk, Image
+import copy
 
 root = tk.Tk()
+
+
+def create_game_plate_arr(game_plate_str):
+    first_row, second_row = "", ""
+    for char in game_plate_str:
+        if char == "_":
+            first_row += "_"
+            second_row += "_"
+        elif char == "G":
+            first_row += "_"
+            second_row += "G"
+        elif char == "L":
+            first_row += "L"
+            second_row += "_"
+        elif char == "M":
+            first_row += "_"
+            second_row += "M"
+    first_row_split = [char for char in first_row]
+    second_row_split = [char for char in second_row]
+    game_plate_arr = [first_row_split, second_row_split]
+    return game_plate_arr
+
+
+test = create_game_plate_arr("__GMG__L")
+print(test)
+
+
+def get_first_state(game_plate_arr):
+    game_plate_arr_copy = copy.deepcopy(game_plate_arr)
+    game_plate_arr_copy[1][0] = "SM"  # super mario
+    return game_plate_arr_copy
+
+
+print(get_first_state(test))
+
+
+def get_sequence_movement(win_path, game_plate_arr):
+    sequence_movement_arr = []
+    super_mario_index = 0
+
+    first_state = get_first_state(win_path, game_plate_arr)
+    sequence_movement_arr.append(first_state)
+
+    for i in len(win_path):
+
 
 
 class GraphicalUserInterface():
@@ -12,7 +58,6 @@ class GraphicalUserInterface():
         self.step = 0
         self.game_plate = game_plate
         self.Visualize()
-
 
     def print_10_next_state(self, index):
         pass
