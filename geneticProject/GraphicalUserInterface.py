b4eb70a1bca1f800be42fb9078a0ec9ceb13e85f
unknown <arminzd1@gmail.com>
2021-06-01 13:45:43 +0430

    test GUI and sucessfully

--- geneticProject/GraphicalUserInterface.py
+++ geneticProject/GraphicalUserInterface.py
@@ -27,7 +27,7 @@ def create_game_plate_arr(game_plate_str):
     return game_plate_arr
 
 
-test = create_game_plate_arr("__G__")
+# test = create_game_plate_arr("__G__")
 
 
 def get_first_state(game_plate_arr):
@@ -56,7 +56,6 @@ def get_sequence_movement(win_path, game_plate_arr):
     while i < len(win_path):
         if win_path[i] == "0":
             current_super_mario_index = get_super_mario_index(sequence_movement_arr[-1], "SM")
-            print(current_super_mario_index)
             next_state = copy.deepcopy(sequence_movement_arr[-1])
             next_state[current_super_mario_index[0]][current_super_mario_index[1]] = "_"
             next_state[1][current_super_mario_index[1] + 1] = "SM"
@@ -86,24 +85,22 @@ def get_sequence_movement(win_path, game_plate_arr):
     return sequence_movement_arr
 
 
-print(get_sequence_movement("0000", test))
-exit()
+# print(get_sequence_movement("0000", test))
+
 
 
 class GraphicalUserInterface():
 
-    def __init__(self, path_string, game_plate):
-        self.path_string = path_string
+    def __init__(self, path):
+        self.path = path
         self.step = 0
-        self.game_plate = game_plate
+        # self.game_plate = game_plate
         self.Visualize()
 
-    def print_10_next_state(self, index):
-        pass
+
 
     def Visualize(self, e=1):
-        num_rows = 2
-        num_cols = len(self.path_string)
+        num_rows, num_cols = 2, 6
 
         WIDTH = (num_cols * 100) + 10
         HEIGHT = (num_rows * 100) + 10
@@ -116,14 +113,55 @@ class GraphicalUserInterface():
 
         super_mario_image = ImageTk.PhotoImage(
             Image.open('./picture_for_gui/superMario.jpg').resize((100, 100), Image.ANTIALIAS))
+        half_super_mario_image = ImageTk.PhotoImage(
+            Image.open('./picture_for_gui/superMario.jpg').resize((50, 100), Image.ANTIALIAS))
         mushroom_image = ImageTk.PhotoImage(
             Image.open('./picture_for_gui/mushrooms.jpg').resize((100, 100), Image.ANTIALIAS))
         ghost_image = ImageTk.PhotoImage(Image.open('./picture_for_gui/ghost.jpg').resize((100, 100), Image.ANTIALIAS))
         gumpa_image = ImageTk.PhotoImage(Image.open('./picture_for_gui/gumpa.jpg').resize((100, 100), Image.ANTIALIAS))
         cell_image = ImageTk.PhotoImage(Image.open('./picture_for_gui/cell.png').resize((100, 100), Image.ANTIALIAS))
 
-        super_mario_label = ttk.Label(root, image=super_mario_image)
-        super_mario_label.grid(column=0, row=1)
+        if self.step < len(self.path):
+            root.geometry(f"{WIDTH}x{HEIGHT}")
+            root.title("goal chromosome!")
+            root.config(bg="white")
+
+            root.columnconfigure(0, weight=num_cols)
+            root.columnconfigure(1, weight=num_rows)
+            super_mario_index = get_super_mario_index(self.path[self.step], "SM")
+            print(super_mario_index)
+            super_mario_index_i, super_mario_index_j = super_mario_index[0], super_mario_index[1] - 1
+            print(self.path[self.step])
+            for r in range(num_rows):
+                for c in range(num_cols):
+                    if self.path[self.step][r][c + super_mario_index_j] == "_":
+                        cell_label = ttk.Label(root, image=cell_image)
+                        cell_label.grid(column=c, row=r)
+
+
+                    elif self.path[self.step][r][c + super_mario_index_j] == "G":
+                        gumpa_label = ttk.Label(root, image=gumpa_image)
+                        gumpa_label.grid(column=c, row=r)
+
+                    elif self.path[self.step][r][c + super_mario_index_j] == "L":
+                        ghost_label = ttk.Label(root, image=ghost_image)
+                        ghost_label.grid(column=c, row=r)
+
+                    elif self.path[self.step][r][c + super_mario_index_j] == "SM":
+                        super_mario_label = ttk.Label(root, image=super_mario_image)
+                        super_mario_label.grid(column=c, row=r)
+
+                    elif self.path[self.step][r][c + super_mario_index_j] == "M":
+                        mushroom_label = ttk.Label(root, image=mushroom_image)
+                        mushroom_label.grid(column=c, row=r)
+
+
+
+            self.step += 1
+            root.after(800, self.Visualize)
+            root.mainloop()
+        # super_mario_label = ttk.Label(root, image=super_mario_image)
+        # super_mario_label.grid(column=0, row=1)
 
         # for i in range(len(self.game_plate)):
         #     print(self.game_plate[i])
@@ -155,7 +193,12 @@ class GraphicalUserInterface():
         #         ghost_label.grid(column=i, row=0)
         #         cell_label.grid(column=i, row=1)
 
-        root.mainloop()
+        # root.mainloop()
 
-
-g = GraphicalUserInterface("001100", "_G_ML_")
+game_plate_arr = create_game_plate_arr("__G___L_" + "______")
+print(game_plate_arr)
+movement = get_sequence_movement("01010200", game_plate_arr)
+print(movement)
+g = GraphicalUserInterface(movement)
+g.Visualize()
+# g = GraphicalUserInterface("001100", "_G_ML_")
