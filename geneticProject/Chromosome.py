5c3869250e1265f07bdae5fd5a8e9ad5545b8b33
Amirhossein Rajabpour <rajabpouramirhosein@gmail.com>
2021-06-01 14:18:23 +0430

    selection algorithm corrected. max and min scores are traceable now

--- geneticProject/Chromosome.py
+++ geneticProject/Chromosome.py
@@ -14,23 +14,21 @@ class Chromosome:
         self.failure_points = new_failurepoints
 
 
-def selection(len_population, array_of_new_chromosomes, array_of_prev_chromosomes, selection_mode):
-    # 70% of new population is from new chormosomes (children of previous layer)
-    # 30% of new population is from chromosomes in the previous layer
-    next_generation = []
-    next_generation += array_of_new_chromosomes
+def selection(array_of_prev_chromosomes, selection_mode):
+    selected_parents = []
+    len_population = len(array_of_prev_chromosomes)
 
     # randomly (with their scores as their weights) select from previous generation
     if selection_mode == 'random':
         score_weights = return_scores(array_of_prev_chromosomes)
-        next_generation += random.choices(list(array_of_prev_chromosomes), weights=score_weights,k=math.ceil(len_population * 0.3))
+        selected_parents += random.choices(list(array_of_prev_chromosomes), weights=score_weights,k=math.ceil(len_population * 0.5))
     
     # sort array_of_prev_chromosomes and take top chromosomes
     elif selection_mode == 'best':
         sorted_array_of_prev_chromosomes = sort_by_score(array_of_prev_chromosomes)
-        next_generation += sorted_array_of_prev_chromosomes[:math.ceil(len_population * 0.3)]
+        selected_parents += sorted_array_of_prev_chromosomes[:math.ceil(len_population * 0.5)]
 
-    return next_generation
+    return selected_parents
 
 
 def crossover(chromosome1, chromosome2, crossover_point, crossover_mode):
--- geneticProject/Genetic.py
+++ geneticProject/Genetic.py
@@ -10,6 +10,14 @@ def calculate_average_score(array_of_chromosomes):
         sum_scores += chromosome.score
     return sum_scores/len(array_of_chromosomes)
 
+def find_max_score(array_of_chromosomes):
+    array_of_chromosomes.sort(key=lambda x: x.score, reverse=True)
+    return array_of_chromosomes[0]
+
+def find_min_score(array_of_chromosomes):
+    array_of_chromosomes.sort(key=lambda x: x.score, reverse=False)[0]
+    return array_of_chromosomes[0]
+
 def choose_two_parents(array_of_chromosomes):
     parent1, parent2 = random.sample(array_of_chromosomes, 2)
     return parent1, parent2
@@ -26,18 +34,29 @@ class Genetic:
         self.crossover_point = crossover_point
         self.best_answer = ''
         self.generation_average_scores = {}
+        self.generation_max_score = {}
+        self.generation_min_score = {}
 
-        current_generation = 2
+
         len_population = len(self.population[1])
         self.generation_average_scores[1] = calculate_average_score(self.population[1])
+        self.generation_max_score[1] = find_max_score(self.population[1])
+        self.generation_min_score[1] = find_min_score(self.population[1])
 
+        current_generation = 2
         while not is_end():
+
+            # 70% of new population are from new chormosomes (children of selected parents)
+            # 30% of new population are from selected parents (after selection step)
+
+            # selection step
+            selected_parents = selection(self.population[current_generation-1], self.selection_mode)
 
             # crossover step
             new_generation, new_generation_strings = [], []
             while True:
                 # half of the population which is better than the other half is chosen and 70% of the new generation are children of this half
-                parent1, parent2 = choose_two_parents(sort_by_score(self.population[current_generation-1])[:math.ceil(len_population * 0.5)])
+                parent1, parent2 = choose_two_parents(selected_parents)
                 child1, child2 = crossover(parent1, parent2, self.crossover_point, self.crossover_mode)
                 game = Game(self.game_plate)
 
@@ -55,14 +74,16 @@ class Genetic:
                 if len(new_generation_strings) == math.ceil(len_population * 0.7):
                     break
 
+            new_generation += selected_parents
 
-            # selection step
-            self.population[current_generation] = selection(len_population, new_generation, self.population[current_generation-1], self.selection_mode)
 
             # mutation step
             for chromosome in self.population[current_generation]:
                 mutation(chromosome, 0.1, game)
 
             self.generation_average_scores[current_generation] = calculate_average_score(self.population[current_generation])
+            self.generation_max_score[current_generation] = find_max_score(self.population[current_generation])
+            self.generation_min_score[current_generation] = find_min_score(self.population[current_generation])
+
             
             current_generation += 1
